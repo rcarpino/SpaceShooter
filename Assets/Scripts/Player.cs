9bb14448c8fa31d7c954aedbd9baefdc8962c006
Richard Carpino <carpino82@gmail.com>
2022-07-10 11:23:49 -0500

    Player Bounds and Constraints

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,34 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //take the current position = new position (0,0,0)
         transform.position = new Vector3(0, 0, 0);
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0),0);
+
+        if (transform.position.x > 11.4f)
+        {
+            transform.position = new Vector3(-11.4f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.4f)
+        {
+            transform.position = new Vector3(11.4f, transform.position.y, 0);
+        }
     }
+
 }
