7cbdfeac171b7e1e55db955d1dbca665fa219972
Richard Carpino <carpino82@gmail.com>
2022-07-10 07:07:21 -0500

    cube added and script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //take the current position = new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+
     }
 
     // Update is called once per frame
