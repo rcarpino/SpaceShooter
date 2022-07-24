72d0057720e57270060843ef44fb47744a851cea
Richard Carpino <carpino82@gmail.com>
2022-07-24 18:54:47 -0500

    Added Main Menu with Button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     {
         if (_isGameOver && Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(0); //Current Game Scene
+            SceneManager.LoadScene(1); //Current Game Scene
         }
     }
 
