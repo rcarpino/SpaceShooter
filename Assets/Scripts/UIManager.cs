9c64d16edd3e982858cf80cdaad78924ff5ada53
Richard Carpino <carpino82@gmail.com>
2022-07-24 17:13:48 -0500

    Implemented GAME OVER flicker

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,7 +17,6 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Text _gameOverText;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -38,7 +37,23 @@ public class UIManager : MonoBehaviour
         if(currentLives == 0)
         {
             _gameOverText.gameObject.SetActive(true);
+            StartCoroutine(GameOverFlickerRoutine());
+
+             
         }
         
     }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while(true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+
 }
