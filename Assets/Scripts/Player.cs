f018f4b67ea044bd3391d4ae3d41ed4430e6c62b
Richard Carpino <carpino82@gmail.com>
2022-07-24 14:40:58 -0500

    Implemented Lives Display

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -115,6 +115,8 @@ public class Player : MonoBehaviour
         }
 
         _lives--;
+        _uiManager.UpdateLives(_lives);
+
 
         if(_lives < 1)
         {
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,17 +7,31 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Text _scoreText;
+    
+    [SerializeField]
+    private Image _LivesImg;
+
+    [SerializeField]
+    private Sprite[] _liveSprites;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        
         _scoreText.text = "Score: " + 0; 
     }
 
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        //display img sprite
+        //give it a new one based on current lives index
+        _LivesImg.sprite = _liveSprites[currentLives];
     }
 }
